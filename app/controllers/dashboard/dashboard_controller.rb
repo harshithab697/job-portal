352289c84dfa1990fd496764b61edde5ba5aa1d6
somak-progton <somak@progton.com>
2019-10-29 15:21:22 +0530

    devise login

--- app/controllers/dashboard/dashboard_controller.rb
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -1,9 +1,5 @@
 class Dashboard::DashboardController < ApplicationController
   def index
-  	if current_user.type == "JobSeeker"
-	
-		elsif current_user.type == "Recruiter"
-			
-		end
+  	
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :recruiters
   devise_for :job_seekers
   namespace :dashboard do
-    # authenticated :job_seeker do
+    authenticated do
     #     # resources :jobs, module: "job_seeker", :only => [:show, :index]
     # end
 
@@ -11,11 +11,12 @@ Rails.application.routes.draw do
     #     # resources :jobs, module: "recruiter"
     # end
 
-    root to: "dashboard#index"
+      root to: "dashboard#index"
+    end
   end
   get 'welcomes/index'
   get 'welcomes/register' => 'welcomes#register'
+  root to: 'welcomes#index'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'welcomes#index'
 end
