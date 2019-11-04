7e013c0e576ef3e605295d111ccb95e361097804
somak-progton <54354797+somak-progton@users.noreply.github.com>
2019-11-04 12:39:31 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
       patch 'profiles/update'
     end
 
-    authenticated :job_seekers do
+    authenticated :job_seeker do
       resources :jobs
       get 'profiles/show'
       get 'profiles/edit'
