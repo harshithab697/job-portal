990ab0debc7ba4209537752d1715fcbf1caae5ed
hakeemasma <asma@progton.com>
2019-11-07 18:42:40 +0530

    displayed all jobs

--- app/controllers/dashboard/dashboard_controller.rb
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -1,4 +1,20 @@
 class Dashboard::DashboardController < ApplicationController
   def index
-  end
+	 if current_user.instance_of?JobSeeker
+	 	 @applied_job = [ ]
+	 	 @all_jobs = [ ]
+	 	 @jobs = JobApplication.where(job_seeker_id: current_user.id)
+	 	 @jobs.each do |job|
+	 	 		@applied_job << job.job_id
+	 	 end
+	 	 if @applied_job.empty?
+	 	 		@all_jobs = Dashboard::Job.all
+	 	 else
+	 	 	  @applied_job.each do |apply|
+	 	 		@all_jobs << Dashboard::Job.where.not(id: apply)
+	 	    end
+	 	  end
+	 else
+	 end
+	end
 end
