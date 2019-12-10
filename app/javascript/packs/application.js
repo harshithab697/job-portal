706f536b59b9edb9ebfc00df0a33f8e124741fec
rajasekhar <raja@progton.com>
2019-12-10 15:40:22 +0530

    Fixed styles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,15 +11,6 @@ import 'bootstrap'
 import '../stylesheets/application'
 //import './src/application.scss'
 require("packs/main")
-require("packs/map-custom")
-require("packs/choices")
-require("packs/custom-materialize")
-require("packs/flatpickr")
-document.addEventListener("turbolinks:load", () => {
- $('[data-toggle="tooltip"]').tooltip()
- $('[data-toggle="popover"]').popover()
- 
-})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
